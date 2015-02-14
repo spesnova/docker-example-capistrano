f2fa0b9cb8404335e1268416c7579704090f3354
Seigo Uchida <spesnova@gmail.com>
2015-02-15 03:03:36 +0900

    Enable to run one-off container

---
+++ capistrano/lib/capistrano/tasks/run.rb
@@ -0,0 +1,38 @@
+desc "Run one-off container for an app"
+task :run do
+  if fetch(:stage) == :default
+    abort "Please specify app to run: $ cap rails-blue run"
+  end
+
+  ask(:command, "/bin/bash", echo: true)
+
+  container   = "#{fetch(:container)}.run.#{Time.now.to_i}"
+  image       = fetch(:image)
+  options     = fetch(:options)
+  command     = fetch(:command)
+
+  docker_command = [
+    "/usr/bin/docker",
+    "run",
+    "-it",
+    "--rm",
+    "--name #{container}",
+    "#{options}",
+    "#{image}",
+    "#{command}",
+  ].join(" ")
+
+  ssh_command = [
+    "ssh",
+    "-t",
+    "-i #{fetch(:ssh_options)[:keys].first}",
+    "-o StrictHostKeyChecking=no",
+    "-o UserKnownHostsFile=/dev/null",
+    "-o LogLevel=quiet",
+    "#{fetch(:ssh_options)[:user]}@#{roles(:cap).first.hostname}",
+    "'#{docker_command}'",
+  ].join(" ")
+
+  puts "Running #{command}... up, #{container}"
+  exec(ssh_command)
+end
